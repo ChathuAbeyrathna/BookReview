07a6b334d09164964e0f5123bdb50c0588eb0a89
M.R.C.D. Abeyrathna <110781133+ChathuAbeyrathna@users.noreply.github.com>
2024-11-29 09:39:05 +0530

    Update Loader.js

--- frontend/src/Component/Loader/Loader.js
+++ frontend/src/Component/Loader/Loader.js
@@ -5,8 +5,7 @@ import octopusImage from '../Assets/polvo-pepe-bando-do-mar.gif';
 export const Loader = () => {
   return (
     <div className="loader-container">
-      <img src={octopusImage} alt="" className="loader-image"/>
-      <p className="loader-text">Hold on, we're tinkering away to bring your page to life...</p>
+      <p className="loader-text">Hold on, This your magic books world...!</p>
     </div>
   );
 }
